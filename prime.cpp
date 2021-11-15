820a3c8d5a6ebe532885c322879c164943817b33
shakshi18 <91950256+shakshi18@users.noreply.github.com>
2021-11-15 11:11:36 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -28,3 +28,37 @@ int main() {
 
     return 0;
 }
+
+// there is a ishu with previous program if we are giving 2 then it will print not prime but 2 is a prime number
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, n;
+    bool isPrime = true;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    // 0 and 1 are not prime numbers
+    if (n == 0 || n == 1) {
+        isPrime = false;
+    }
+    else if (n==2){
+        isPrime =true;
+    }
+    else {
+        for (i = 2; i <= n ; ++i) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime)
+        cout << n << " is a prime number";
+    else
+        cout << n << " is not a prime number";
+
+    return 0;
+}
