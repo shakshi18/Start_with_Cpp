bc3ae751278de78216c8542ac9322c2504646872
shakshi18 <91950256+shakshi18@users.noreply.github.com>
2021-11-15 17:21:58 +0530

    Update P1.cpp

--- pattern/P1.cpp
+++ pattern/P1.cpp
@@ -1,1 +1,24 @@
+/*
+1
+12
+123
+1234
+12345*/
 
+#include<iostream>
+using namespace  std;
+int main(){
+    int n;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
