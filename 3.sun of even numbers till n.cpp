66cdae43e6bc96296ea960678f9bed104bd2d92b
shakshi18 <91950256+shakshi18@users.noreply.github.com>
2021-11-15 12:35:28 +0530

    Update and rename 3.sun of even numbers till n to 3.sun of even numbers till n.cpp

---
+++ 3.sun of even numbers till n.cpp
@@ -0,0 +1,46 @@
+// using while loop
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n ,sum=0;
+    int i =1;
+    cin>>n;
+    while(i<=n){
+        if(i%2==0){
+            sum=sum+i;
+        }
+        i++;
+    }
+        cout<<"the total sun is"<<sum;
+
+}
+
+// using for loop
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i, n, sum = 0;
+
+    // Take input from user.
+    cout << "Print sum of even numbers till : ";
+    cin >> n;
+
+    for(i = 1; i <= n; i++) {
+
+        // Check for even or not.
+        if((i % 2) == 0) {
+
+            sum += i;
+
+        }
+    }
+
+    cout << endl << "Sum of even numbers from 1 to " << n << " is : " << sum;
+
+    return 0;
+}
