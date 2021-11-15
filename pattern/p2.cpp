336b95d91de6f2aeb434637165aac28669b471da
shakshi18 <91950256+shakshi18@users.noreply.github.com>
2021-11-15 17:37:52 +0530

    Update p2.cpp

--- pattern/p2.cpp
+++ pattern/p2.cpp
@@ -1,1 +1,25 @@
+/*
+1
+23
+456
+78910*/
 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    int i=1;
+    int val =1;
+    while (i<=n){
+        int j=1;
+        while (j<=i){
+            cout<<val;
+            val++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
